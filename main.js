9709227f8bfdeb223122cd71535481b844e7da4c
PrateekShukla <124058592+Prateek12code@users.noreply.github.com>
2023-12-13 18:46:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,17 +18,27 @@ recognition.onresult = function(event) {
 
     document.getElementById("textbox").innerHTML = content;
 
-    speak();
+    if (content == "take my selfie")
+    {
+        console.log("taking selfie......");
+        speak();
+    }
 
 }
 
 function speak(){
-    var synth = window.SpeechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    var synth = window.speechSynthesis;
+    speak_data = "Taking Your Selfie In 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    },5000);
 }
 
 
@@ -40,3 +50,16 @@ Webcam.set({
     image_format: 'png',
     png_quality:90
 });
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
